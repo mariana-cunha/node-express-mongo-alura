71c15955232880d00853ddd4220c051982d81ab2
Mariana Cunha <mari.marques26@gmail.com>
2025-11-07 16:16:03 -0300

    create express app, replaces server http

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-import http from "http";
+// import http from "http";
+import app from './src/app.js';
 
 const PORT = 3000;
 
@@ -8,11 +9,13 @@ const rotas = {
     "/autores": "entrei na rota autores",
 };
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(rotas[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+
+// server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("servidor escutando..")
 });
