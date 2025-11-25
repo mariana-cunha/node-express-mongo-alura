fcd44bc5d5ed500d14fcc7d24847b672d3057f6a
Mariana Cunha <mari.marques26@gmail.com>
2025-11-25 16:48:47 -0300

    unite livros-books and authors

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -26,11 +27,20 @@ class LivroController {
     };
 
     static async cadastrarLivro (req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body)
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = {
+                ...novoLivro,
+                autor: {
+                    ...autorEncontrado._doc,
+                }
+            }
+            const livroCriado = await livro.create(livroCompleto);
             res.status(201).json({
                 message: "livro cadastrado com sucesso",
-                livro: novoLivro
+                livro: livroCriado
             })
         } catch (error) {
             res.status(500).json({
